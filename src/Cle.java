cfe004dfc806b64a23134a49feb40a7ca4c49752
Chantal Vignola <adelepom@outlook.com>
2024-02-09 21:15:34 -0500

    c est un test

--- src/Cle.java
+++ src/Cle.java
@@ -1,20 +1,26 @@
-/*public class Cle extends Case {
-    private Point position;
+public class Cle extends Case {
+private Point position;
+	
+	public Cle(Point position) {
+		this.position = position;
+	}
 
-    public Cle(Point position) {
-        this.position = position;
+	@Override
+	public Boolean interactionPossible(Robot robot) {
+		return true;
+	}
+
+	@Override
+	public void interagir(Robot robot) {
+		// Il faut créer un getter et setter nbCles dans la classe Robot
+		int nbCles = robot.getNbCles() + 1;
+		robot.setNbCles(nbCles);
+	} 
+	
+	@Override
+    public char getRepresentation() {
+		this.representation = '\'';
+        return representation;
     }
-
-    @Override
-    public Boolean interactionPossible(Robot robot) {
-        return true;
-    }
-
-    @Override
-    public void interagir(Robot robot) {
-        // Il faut créer un getter et setter nbCles dans la classe Robot
-        int nbCles = robot.getNbCles() + 1;
-        robot.setNbCles(nbCles);
-    }
-
-}*/
+	
+}
