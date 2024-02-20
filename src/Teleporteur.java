f6cf49be334ab34d7522b6cfa37f9ba846b8cccb
elle-cc <159561371+elle-cc@users.noreply.github.com>
2024-02-20 04:04:43 -0500

    javadoc Update Teleporteur.java

--- src/Teleporteur.java
+++ src/Teleporteur.java
@@ -1,15 +1,33 @@
+/**
+ * Classe qui hérite de 'Case'
+ *
+ * @author Adèle Pomerleau
+ * @author Adel Tayeb Boudia
+ * @author Christelle Semaan
+ */
 public class Teleporteur extends Case{
-	
+	/**
+	 * Constructeur qui initialise un téléporteur.
+	 * lorsqu'affiché visuellement dans la grille de jeu, il est représenté par le charactère 'T'.
+	 */
 	public Teleporteur() {
 		super.representation = 'T';
 	}
 
+	/**
+	 * Le robot peut toujours interragir avec le téléporteur.
+	 * @param robot Objet
+	 * @return True
+	 */
 	@Override
-	// Le robot peut toujours interragir avec le téléporteur
 	public Boolean interactionPossible(Robot robot) {
 		return true;
 	}
 
+	/**
+	 * Le robot a trouvé le téléporteur.
+	 * @param robot Objet
+	 */
 	@Override
 	public void interagir(Robot robot) {
 		robot.setTeleporteur(true);
