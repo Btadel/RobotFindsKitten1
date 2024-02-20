f9021ec1c6055575515aa469fae60b446d41517f
elle-cc <159561371+elle-cc@users.noreply.github.com>
2024-02-20 06:26:20 -0500

    javadoc Update Mur.java

--- src/Mur.java
+++ src/Mur.java
@@ -1,19 +1,39 @@
+/**
+ * classe qui hérite de 'Case'
+ *
+ * @author Adèle Pmerleau
+ * @author Adel Tayeb Boudia
+ * @author Christelle Semaan
+ */
 public class Mur extends Case {
-	
-	// Les objets de type Mur ne prennent aucun attribut de plus que les cases.
+
+	/**
+	 * Les objets de type Mur ne prennent aucun attribut de plus que les cases.
+	 * Est repésenté par l'objet '%'.
+	 */
 	public Mur() {
 		this.representation = '%';
 	}
 
+	/**
+	 * Ils ne peuvent pas interragir avec le robot.
+	 *
+	 * @param robot Objet qui n'interragit pas avec le mur
+	 */
 	@Override
-	// Ils ne peuvent pas interragir avec le robot
 	public Boolean interactionPossible(Robot robot) {
 		return false;
 	}
 
+	/**
+	 * Aucune intéraction entre le mur et le robot.
+	 * Ne retourne rien.
+	 *
+	 * @param robot Objet qui n'interragit pas avec le mur
+	 */
 	@Override
 	public void interagir(Robot robot) {
-		return;	
+		return;
 	}
 
 }
