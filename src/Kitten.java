e7bd0a09c522961d5dc9c6d245a49bb05b5cc949
elle-cc <159561371+elle-cc@users.noreply.github.com>
2024-02-20 06:09:58 -0500

    javadoc Update Kitten.java

--- src/Kitten.java
+++ src/Kitten.java
@@ -1,22 +1,42 @@
+/**
+ * classe qui hérite de 'Case'
+ *
+ * @author Adèle Pmerleau
+ * @author Adel Tayeb Boudia
+ * @author Christelle Semaan
+ */
 public class Kitten extends Case{
-	
+
 	/* Les objets de type kitten prennent un seul type de plus que ceux de type case : un string représentant le nom 
 	du Kitten*/
 	private String nom;
-	
+
+	/**
+	 * Initialise le Kitten avec un nom et un symbol aléatoire.
+	 *
+	 * @param nom Le nom du kitten
+	 */
 	public Kitten(String nom) {
 		this.nom = nom;
 		super.representation = getRandomSymbole();
 	}
 
+	/**
+	 * Peut interragir avec le robot
+	 *
+	 * @param robot
+	 */
 	@Override
-	// Peut interragir avec le robot
 	public Boolean interactionPossible(Robot robot) {
 		return true;
 	}
 
+	/**
+	 *Interaction entre le Kitten et le robot : le jeu a été gagné!
+	 *
+	 * @param robot
+	 */
 	@Override
-	// Interaction entre le Kitten et le robot : le jeu a été gagné!
 	public void interagir(Robot robot) {
 		System.out.println("You found kitten! Way to go, robot.\n" + this.nom + " <3 " + robot.getNom());
 	}
