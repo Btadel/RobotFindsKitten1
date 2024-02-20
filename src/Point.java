de3edd3488bf64c108b130d50476db20e520d125
elle-cc <159561371+elle-cc@users.noreply.github.com>
2024-02-20 05:06:12 -0500

    Ajout d'un javadoc au code de la prof  Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,22 +1,55 @@
 import java.util.*;
-
+/**
+ * représentation d'un point (x,y)
+ *
+ * @author Profresseur
+ * <p>
+ *     Modifications apportées par:
+ * <p>
+ * @author Adèle Pmerleau
+ * @author Adel Tayeb Boudia
+ * @author Christelle Semaan
+ */
 public class Point {
+
     //ATTRIBUTS
     private final int x, y;
+
     //CONSTRUCTEUR
+    /**
+     * Initialise un point avec des coordonées (x,y) spécifique.
+     * @param x coordonée en x du point
+     * @param y coordonée en y du point
+     */
     public	Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     //METHODES
+    /**
+     * Vérifie si les coordonées du point concordent avec 'x' et 'y'.
+     * @param x coordonée en x du point
+     * @param y coordonée en y du point
+     * @return True si égale, sinon False
+     */
     public boolean egal(int x, int y) {
         return x == this.x && y == this.y;
                
     }
-    
+
+    /**
+     * Méthode qui retourne la coordonée x du point.
+     * @return x
+     */
     public int getX() {
         return x;
     }
+
+    /**
+     * Méthode qui retourne la coordonée x du point.
+     * @return y
+     */
     public int getY() {
         return y;
     }
