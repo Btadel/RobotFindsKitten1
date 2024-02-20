c29d2d85b0051d530e48c2b10eb88a29c12011f6
elle-cc <159561371+elle-cc@users.noreply.github.com>
2024-02-20 07:10:22 -0500

    javadoc Update Porte.java

--- src/Porte.java
+++ src/Porte.java
@@ -1,46 +1,69 @@
+/**
+ * classe qui hérite de 'Case'
+ *
+ * @author Adèle Pmerleau
+ * @author Adel Tayeb Boudia
+ * @author Christelle Semaan
+ */
 public class Porte extends Case{
 	
 	/*Les objets de type Porte ne prennent qu'un attribut de plus que case :
 	le booléen état qui représente si la porte est ouverte ou fermée (true = ouvert, false = fermée) */
-	
-	private boolean etat; 
-	
+
+	private boolean etat;
+
+	/**
+	 * Génère une porte initialement fermée.
+	 * Défini par le symbole '!'.
+	 */
 	public Porte() {
 		this.etat = false;
 		this.representation = '!';
 	}
-	
+
+	/**
+	 * Le robot ne peut interragir avec une porte que s'il a une clé ou si la porte est déjà ouverte.
+	 * @param robot Objet qui intéragit avec la porte.
+	 * @return True si une intéraction est possible, sinon False.
+	 */
 	@Override
 	public Boolean interactionPossible(Robot robot) {
-		
-		// Le robot ne peut interragir avec une porte que s'il a une clé ou si la porte est déjà ouverte
+
 		if (robot.getNbCles() != 0 || this.etat == true) {
 			return true;
 		}
 		else {
-		return false;
+			return false;
 		}
 	}
 
+	/**
+	 * les deux manière dont peut interagir un robot avec une porte.
+	 * Si elle est déja ouverte ou s'il ouvre la porte.
+	 * @param robot Objet qui intéragit avec la porte.
+	 */
 	@Override
 	public void interagir(Robot robot) {
-		
+
 		// Le robot ouvre la porte, elle devient ouverte
 		if (this.etat == false) {
-			this.etat = true; 	
-		
-		// La clé a été utilisée
+			this.etat = true;
+
+			// La clé a été utilisée
 			int nbCles = robot.getNbCles()-1;
 			robot.setNbCles(nbCles);
 		}
 		else {
 			return;
 		}
-		
+
 	}
-	
+
+	/**
+	 * Indique si la porte est fermé ou ouverte.
+	 */
 	public boolean getEtat() {
 		return this.etat;
 	}
-	
+
 }
